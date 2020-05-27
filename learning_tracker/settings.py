4d0739003a86c4027bf5f21fc0a78625cf10b1cb
Sivaramakrishna Allam <siva.allam94@gmail.com>
2020-05-27 12:27:54 +0530

    added procfile.windows

--- learning_tracker/settings.py
+++ learning_tracker/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'learning_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
